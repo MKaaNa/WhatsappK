99bd09fe794f0557e644ae82e801956e99c9d775
MKaaNa <mkaanaltin@gmail.com>
2024-07-06 21:10:34 +0300

    Initial Commit

---
+++ WhatsappK/WhatsappKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappKApp.swift
+//  WhatsappK
+//
+//  Created by Kaan Mac Pro on 6.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
